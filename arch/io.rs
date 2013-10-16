41126b621292f07753bb3f17615a235bd52bd23c
Léo Testard <leo.testard@gmail.com>
2013-10-16 10:09:09 +0200

    Add simple text IO

---
+++ arch/io.rs
@@ -0,0 +1,9 @@
+#[cfg(target_arch = "x86_64")]
+
+pub use self::x86_64::io::*;
+
+mod x86_64
+{
+    mod io;
+}
+
--- entry.rs
+++ entry.rs
@@ -4,23 +4,14 @@
 #[no_std];
 #[no_core];
 
+mod util;
+mod arch;
+mod runtime;
+
 #[no_mangle]
 pub unsafe fn main ()
 {
-    *((0xB8000) as *mut char) = 'Q';
-    *((0xB8001) as *mut u8) = 15;
-    *((0xB8002) as *mut char) = 'U';
-    *((0xB8003) as *mut u8) = 15;
-    *((0xB8004) as *mut char) = 'A';
-    *((0xB8005) as *mut u8) = 15;
-    *((0xB8006) as *mut char) = 'S';
-    *((0xB8007) as *mut u8) = 15;
-    *((0xB8008) as *mut char) = 'A';
-    *((0xB8009) as *mut u8) = 15;
-    *((0xB800A) as *mut char) = 'R';
-    *((0xB800B) as *mut u8) = 15;
+    use util::kprintln;
 
-    loop
-    {
-    }
+    kprintln("QUASAR");
 }
